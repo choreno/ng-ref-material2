d7be163bdddde494243964ff9358040e0eae2758
THBar <choreno@gmail.com>
2017-01-09 15:10:07 -0500

    installed @angular/flex-layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+
+//angular material2
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+
 import { AppComponent } from './app.component';
 
 @NgModule({
